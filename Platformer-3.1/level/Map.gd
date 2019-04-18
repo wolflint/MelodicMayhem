271ca9b3d2bcfb18bada651b722fb804b9b82bb0
Marcin Wolf <mrcnwolf@gmail.com>
2019-04-18 13:28:05 +0100

    Added basic high score dictionary

--- Platformer-3.1/level/Map.gd
+++ Platformer-3.1/level/Map.gd
@@ -9,44 +9,46 @@ signal personal_best_changed(personal_best)
 
 export (bool) var is_dungeon = false
 export (String) var MAP_NAME
-var highscore : int
+
 var personal_best : int
 var score = 0
 
 func _ready():
 	connect("score_changed", GAME, "_on_Map_score_changed", [score])
 	connect("personal_best_changed", GAME, "_on_Map_personal_best_changed", [personal_best])
+	print(highscores)
+	print(highscores.get('map name', "Doesn't exist").items())
 
-func _update_highscore(new_score):
-	if new_score > highscore:
-		highscore = new_score
-	if new_score > personal_best:
-		personal_best = new_score
-		emit_signal("personal_best_changed", personal_best)
-
-func _on_enemy_died(score_worth):
-	score += score_worth
-	emit_signal("score_changed", score)
-	_update_highscore(score)
-	print(score)
-
-### HIGHSCORE SYSTEM ###
-func get_score_data():
-	return {
-		"filename": filename,
-		"parent": get_parent().get_path(),
-		"properties": {
-			"MAP_NAME": MAP_NAME,
-			"highscore": highscore,
-		}
-	}
-
-func get_save_data():
-	return {
-		"filename": filename,
-		"parent": get_parent().get_path(),
-		"properties": {
-			"MAP_NAME": MAP_NAME,
-			"personal_best": personal_best,
-		}
-	}
+#func _update_highscore(new_score):
+#	if new_score > highscore:
+#		highscore = new_score
+#	if new_score > personal_best:
+#		personal_best = new_score
+#		emit_signal("personal_best_changed", personal_best)
+#
+#func _on_enemy_died(score_worth):
+#	score += score_worth
+#	emit_signal("score_changed", score)
+#	_update_highscore(score)
+#	print(score)
+#
+#### HIGHSCORE SYSTEM ###
+#func get_score_data():
+#	return {
+#		"filename": filename,
+#		"parent": get_parent().get_path(),
+#		"properties": {
+#			"MAP_NAME": MAP_NAME,
+#			"highscore": highscore,
+#		}
+#	}
+#
+#func get_save_data():
+#	return {
+#		"filename": filename,
+#		"parent": get_parent().get_path(),
+#		"properties": {
+#			"MAP_NAME": MAP_NAME,
+#			"personal_best": personal_best,
+#		}
+#	}
---
+++ Platformer-3.1/save/HighScoreSystem.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var highscores = {}
+
+func _ready():
+	add_highscore('map1', 'marcin', 100)
+	print(add_highscore('map1', 'marcin', 110))
+	
+	print(get_highscore('map1', 'marcin'))
+
+func add_highscore(map_name, player : String, score : int):
+	if map_name in highscores.keys():
+		if player in highscores[map_name].keys():
+			return "Name is already taken"
+	
+	highscores[map_name] = {player : score}
+
+func get_highscore(map_name, player_name):
+	if not map_name in highscores:
+		return 'Map not found in database.'
+	if not player_name in highscores.get(map_name).keys():
+		return "Player hasn't set a high score."
+	return highscores.get(map_name)[player_name]
