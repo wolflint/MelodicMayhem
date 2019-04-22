9755150a17e73c0c474cc3f8521200b450fa47a6
Marcin Wolf <mrcnwolf@gmail.com>
2019-04-22 22:13:05 +0100

    removed test data from highscore menu
    commented it out but need to remove later

--- Platformer-3.1/interface/HighscoreMenu.gd
+++ Platformer-3.1/interface/HighscoreMenu.gd
@@ -2,24 +2,24 @@ extends "res://interface/Menu.gd"
 
 var highscore_text = ""
 
-func _ready():
-	HighScoreSystem.add_highscore("map_name", "player", 100)
-	HighScoreSystem.add_highscore("map_name", "player2", 100)
-	HighScoreSystem.add_highscore("map_name", "player3", 100)
-	HighScoreSystem.add_highscore("map_name", "player4", 100)
-	HighScoreSystem.add_highscore("map_name", "player5", 100)
-	HighScoreSystem.add_highscore("map_name", "player6", 100)
-	HighScoreSystem.add_highscore("map_name", "player7", 100)
-	HighScoreSystem.add_highscore("map_name", "player8", 100)
-	HighScoreSystem.add_highscore("map_name", "player9", 100)
-	HighScoreSystem.add_highscore("map_name", "player10", 100)
-	HighScoreSystem.add_highscore("map_name", "player11", 100)
-	HighScoreSystem.add_highscore("map_name", "player12", 100)
-	HighScoreSystem.add_highscore("map_name", "player13", 100)
-	HighScoreSystem.add_highscore("map_name", "player14", 100)
-	HighScoreSystem.add_highscore("map_name", "player15", 100)
-	HighScoreSystem.add_highscore("map_name2", "player", 100)
-	HighScoreSystem.add_highscore("map_name2", "player2", 100)
+#func _ready():
+#	HighScoreSystem.add_highscore("map_name", "player", 100)
+#	HighScoreSystem.add_highscore("map_name", "player2", 100)
+#	HighScoreSystem.add_highscore("map_name", "player3", 100)
+#	HighScoreSystem.add_highscore("map_name", "player4", 100)
+#	HighScoreSystem.add_highscore("map_name", "player5", 100)
+#	HighScoreSystem.add_highscore("map_name", "player6", 100)
+#	HighScoreSystem.add_highscore("map_name", "player7", 100)
+#	HighScoreSystem.add_highscore("map_name", "player8", 100)
+#	HighScoreSystem.add_highscore("map_name", "player9", 100)
+#	HighScoreSystem.add_highscore("map_name", "player10", 100)
+#	HighScoreSystem.add_highscore("map_name", "player11", 100)
+#	HighScoreSystem.add_highscore("map_name", "player12", 100)
+#	HighScoreSystem.add_highscore("map_name", "player13", 100)
+#	HighScoreSystem.add_highscore("map_name", "player14", 100)
+#	HighScoreSystem.add_highscore("map_name", "player15", 100)
+#	HighScoreSystem.add_highscore("map_name2", "player", 100)
+#	HighScoreSystem.add_highscore("map_name2", "player2", 100)
 
 func initialize(args = []):
 	for level in HighScoreSystem.highscores.keys():
